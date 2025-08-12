63a95d0f43e66288218d5ac97aa929e955dda25a
DSM3 <fatec-dsm3@academico.intra>
2025-08-12 20:07:20 -0300

    First commit

---
+++ aula-01-criando-API/app.js
@@ -0,0 +1,47 @@
+import express from "express";
+
+const app = express();
+
+//Configurações do express
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Criando um retorno da API
+app.get("/", (req, res) => {
+  const games = [
+    {
+      title: "Delta",
+      year: 2024,
+      genre: "FPS",
+      plataform: "PC (Windows)",
+      price: 0,
+    },
+    {
+      title: "DiabloIII",
+      year: 2012,
+      genre: "RPG",
+      plataform: "PC (Windows)",
+      price: 0,
+    },
+
+    {
+      title: "League of Legends",
+      year: 2009,
+      genre: "MOBA",
+      plataform: "PC (Windows)",
+      price: 0,
+    },
+  ];
+  res.json(games);
+});
+
+//Rodando api na porta 4000
+
+app.listen(4000, (error) => {
+  if (error) {
+    console.log("Ocorreu um erro " + error);
+  } else {
+    console.log(`API rodando em http://localhost:${4000}`);
+  }
+});
