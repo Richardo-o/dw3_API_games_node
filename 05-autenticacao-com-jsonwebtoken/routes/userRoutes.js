48a68f10af4cb63d7a9eab744e485c65074c647a
DSM3 <fatec-dsm3@academico.intra>
2025-09-03 22:23:03 -0300

    atualizando

---
+++ 05-autenticacao-com-jsonwebtoken/routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const userRoutes = express.Router();
+import userController from '../controllers/userController.js';
+
+// Rota para CADASTRAR um usuário
+
+userRoutes.post("/user", userController.createUser)
+
+
+export default userRoutes;
---
+++ 05-autenticacao-com-jsonwebtoken/services/userService.js
@@ -0,0 +1,21 @@
+// Import o model
+import User from "../models/Users.js"
+
+
+class userService{
+    // Método para CADASTRAR usuário
+    async Create(name, email, password){
+    try{
+        const newUser = new User({
+            name,
+            email,
+            password,
+
+        })
+        await newUser.save();
+    }catch(error){
+        console.log(error)
+    }
+}
+}
+export default new userService
